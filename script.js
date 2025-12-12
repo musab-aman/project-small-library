bb07338fac698ed43e2dbffb080062b6287b139d
musabaman11 <musabaman11@gmail.com>
2025-12-12 21:15:03 +0300

    feat: Add function to display books on page
    Implements the `displayBooks` function to loop through `myLibrary` and render each book as a card in the DOM.

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function addBooks(title, author, pages) {
      myLibrary.push(book);
 
 }
+
+function displayBooks() {
+
+    const container = document.getElementById('container-id');
+    container.textContent = "";
+
+    for (const book of myLibrary) {
+    const booksCard = document.createElement('div');
+    booksCard.setAttribute('id', book.id);
+    booksCard.classList.add('card');
+
+    const titleDiv = document.createElement('div');
+    titleDiv.textContent = `title: ${book.title}`;
+    titleDiv.classList.add('title-class');
+    bookscard.appendChild(titleDiv);
+
+    const authorDiv = document.createElement('div');
+    authorDiv.textContent = `author: ${book.author}`;
+    authorDiv.classList.add('author-class')
+    bookscard.appendChild(authorDiv);
+
+    const pageDiv = document.createElement('div');
+    pageDiv.textContent = `pages: ${book.pages}`;
+    pageDiv.classList.add('page-class');
+    bookscard.appendChild(pageDiv);
+
+    container.appendChild(booksCard);
+}
+}
